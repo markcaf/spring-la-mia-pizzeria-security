05530ac7d06c1537a9f57348a24362d29921be2d
Marco Cafasso <marcocafassog@gmail.com>
2022-12-16 10:25:30 +0100

    feat: adds user ecosystem - pojo, repo and service
    - adds many to many relation from user to role in pojo
    - add findByUsername method in the repo
    - adds save, delete, findById and findAll methods in the service

---
+++ Pizzeria/src/main/java/org/generation/italy/demo/serv/UserService.java
@@ -0,0 +1,33 @@
+package org.generation.italy.demo.serv;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.italy.demo.pojo.User;
+import org.generation.italy.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	public void save(User user) {
+		userRepo.save(user);
+	}
+
+	public void delete(User user) {
+		userRepo.delete(user);
+	}
+
+	public Optional<User> findById(int id){
+		return userRepo.findById(id);
+	}
+
+	public List<User> findAll() {
+		return userRepo.findAll();
+	}
+	
+}
